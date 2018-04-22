423e340b41fd9458a891b646fe42316e17983757
Kero <ywillems96@gmail.com>
2018-04-22 14:49:48 +0200

    Changed port log

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const envPort = process.env.PORT;
 
 app.use('/', express.static('assets'));
 app.use('/', require('./shopping_routes/routes'));
 
 app.set('view engine', 'ejs');
 
-app.listen(process.env.PORT || port, () => {
-    console.log(`Listening to port ${port}`);
+app.listen(envPort || port, () => {
+    console.log(`Listening to port ${envPort}`);
 });
