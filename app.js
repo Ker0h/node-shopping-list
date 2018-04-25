4292dfec0c8dc1dc30ab00e9ed6a5316d72d702a
Yannick Willems <ywillems96@gmail.com>
2018-04-25 14:39:51 +0200

    fix

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 8080;
-const envPort = process.env.PORT;
+const port = 8080 || process.env.PORT;
 
 app.use('/', express.static('assets'));
 app.use('/', require('./shopping_routes/routes'));
 
 app.set('view engine', 'ejs');
 
-app.listen(envPort || port, () => {
-    console.log(`Listening to port ${envPort}`);
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
 });
