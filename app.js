88bd2bcc299995a73d0c4c023066097f07f316e5
Yannick Willems <ywillems96@gmail.com>
2018-05-07 11:42:13 +0200

    Changed port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080 || process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.use('/', express.static('assets'));
 app.use('/', require('./shopping_routes/routes'));
